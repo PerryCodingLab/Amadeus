119ee97d48f7248cc23af7d3bd84d67cf2277140
PerryCode <perrymail8@gmail.com>
2026-01-14 14:03:55 +0200

    needs to make datasetcreation into a function

---
+++ src/processing/MidiDataSet.py
@@ -0,0 +1,9 @@
+from MidiDataProcess import process_midi
+from torch.utils.data import Dataset
+from pathlib import Path
+from miditok import REMI
+from miditok.pytorch_data import DatasetMIDI
+from configs.config import MIDI_DATA_DIR
+
+def CreateDataSet():
+    pass
