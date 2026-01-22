603a729af5087b0ae8aabc5343be2559c152c494
PerryCode <perrymail8@gmail.com>
2026-01-22 18:02:16 +0200

    filters

--- src/Tokenizers/TokenizeDataset.py
+++ src/Tokenizers/TokenizeDataset.py
@@ -1,26 +1,25 @@
 from miditok import REMI
 from pathlib import Path
+from processing.MidiDataSet import getMidiByMetaData
 from configs.config import TOKENIZER_PATH, MIDI_DATA_DIR, OUT_DIR
 from tqdm import tqdm #only for visual
 import sys
 
 
-def tokenizeData(tokenizerName, outputFolderName='temp'):
+def tokenizeData(tokenizerName, outputFolderName='temp', genre_keywords=None):
     tokenizer = REMI(params=Path(TOKENIZER_PATH / tokenizerName))
     out_path_folder = OUT_DIR / outputFolderName
     Path(out_path_folder).mkdir(parents=True, exist_ok=True)
 
-    midi_files = Path(MIDI_DATA_DIR).glob("**/*.midi")
+    midi_files = getMidiByMetaData((MIDI_DATA_DIR).glob("**/*.midi"), genre_keywords=genre_keywords)
 
-    count = 0
+
     for midi_path in tqdm(midi_files):
         tokens = tokenizer(midi_path)
         out_path = Path(out_path_folder, midi_path.stem + ".json")
 
         tokenizer.save_tokens(tokens, out_path)
-        count+=1
-        if count == 5:
-            return
+        
 
 
 
